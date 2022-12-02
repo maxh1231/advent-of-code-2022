ca73b68f36e5f0a6ac4781c24a0398f61c504550
Max Humpherys <maxhumpherys@gmail.com>
2022-12-01 20:36:40 -0700

    small refactor

--- day1/index.js
+++ day1/index.js
@@ -1,24 +1,14 @@
 const fs = require('fs')
 let numberArr = fs.readFileSync("./input.txt", "utf-8").split("\n").map(Number);
 let newArr = []
-function calculate(arr) {
-    let count = 0;
-    let groupCount = 1;
-    for (let i = 0; i < arr.length; i++) {
-        count = count + arr[i]
-        if (arr[i] === 0) {
-            newArr.push({ groupCount, count })
-            groupCount++;
-            count = 0;
-        }
+let count = 0;
+for (let i = 0; i < numberArr.length; i++) {
+    count += numberArr[i]
+    if (numberArr[i] === 0) {
+        newArr.push(count)
+        count = 0;
     }
-    return newArr
 }
-let calculatedArr = calculate(numberArr);
-const max = calculatedArr.reduce(function (prev, current) {
-    return (prev.count > current.count) ? prev : current
-})
-const sortedArr = calculatedArr.sort((a, b) => b.count - a.count);
-const sumOfThreeHighest = sortedArr[0].count + sortedArr[1].count + sortedArr[2].count
-console.log(`Highest Calories: ${max.count}, Sum of 3 Highest Elves: ${sumOfThreeHighest}`)
-
+const sortedArr = newArr.sort((a, b) => b - a);
+const sumOfThreeHighest = sortedArr[0] + sortedArr[1] + sortedArr[2]
+console.log(`Highest Calories: ${sortedArr[0]}, Sum of 3 Highest Elves: ${sumOfThreeHighest}`)
